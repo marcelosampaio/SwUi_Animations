d33f4b4255ae41b649e6d77ff52c8a3d7f73919c
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-05-16 15:20:17 -0300

    Initial Commit

---
+++ SwUi_Animations/SwUi_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwUi_AnimationsApp.swift
+//  SwUi_Animations
+//
+//  Created by Marcelo Sampaio on 16/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwUi_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
